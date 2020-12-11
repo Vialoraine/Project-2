922a24655c3184a604953d1e2f2cabccd7454fb0
Nicole Uva <nicouva@gmail.com>
2020-12-10 23:28:12 -0500

    syntax

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,26 @@
 const router = require('express').Router()
-const { PocketUsers } = require('../models')
+const { PocketUser } = require('../models')
 
 router.get('/lists', (req, res) => {
-  PocketUsers.findAll()
+  PocketUser.findAll()
     .then(lists => res.json(lists))
     .catch(err => console.log(err))
 })
 
 router.post('/lists', (req, res) => {
-  PocketUsers.create(req.body)
+  PocketUser.create(req.body)
     .then(list => res.json(list))
     .catch(err => console.log(err))
 })
 
 router.put('/lists/:id', (req, res) => {
-  PocketUsers.update(req.body, { where: { id: req.params.id } })
+  PocketUser.update(req.body, { where: { id: req.params.id } })
     .then(() => res.sendStatus(200))
     .catch(err => console.log(err))
 })
 
 router.delete('/lists/:id', (req, res) => {
-  PocketUsers.destroy({ where: { id: req.params.id } })
+  PocketUser.destroy({ where: { id: req.params.id } })
     .then(() => res.sendStatus(200))
     .catch(err => console.log(err))
 })
